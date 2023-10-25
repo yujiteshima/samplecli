dc845f75605a9f310a6dd1a9c8d4c0fcfddf5ca1
yujiteshima <yuji.teshima@gmail.com>
2023-10-26 05:24:18 +0900

    use clap(derive)

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,32 @@
-use clap::{App, Arg};
+use clap::Parser;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+#[derive(Parser, Debug)]
+#[clap(
+    name = "My RPN program\
+    ",
+    version = "1.0.0",
+    author = "Your name",
+    about = "Super awesome sample RPN calculator",
+)]
+struct Opts {
+    #[clap(short, long)]
+    verbose: bool,
+    #[clap(name = "FILE")]
+    formula_file: Option<String>,
+}
 fn main() {
-    let matches = App::new("My RPN program")
-        .version("1.0.0")
-        .author("Your name")
-        .about("Super awesome sample RPN calculator")
-        .arg(
-            Arg::new("formula_file")
-                .value_name("FILE")
-                .index(1)
-                .required(false),
-        )
-        .arg(
-            Arg::new("verbose")
-                .short('v')
-                .long("verbose")
-                .required(false),
-        )
-        .get_matches();
-    match matches.value_of("formula_file") {
-        Some(file) => println!("File specified: {}", file),
-        None => println!("No file specified."),
+    let opts = Opts::parse();
+
+    if let Some(path) = opts.formula_file {
+        let f = File::open(path).unwrap();
+        let reader = BufReader::new(f);
+
+        for line in reader.lines() {
+            let line = line.unwrap();
+            println!("{}", line);
+        }
+    } else {
+        println!("No file is specified");
     }
-    let verbose = matches.is_present("verbose");
-    println!("Is verbosity specified?: {}", verbose);
 }
